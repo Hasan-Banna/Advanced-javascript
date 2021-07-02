262bbf56ffbe6b8886d8c87ddebc12ec2d47764a
Hasan Al Banna <hasanbanna40@gmail.com>
2021-07-02 21:52:13 +0600

    double and triple equal uses

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,9 @@
+//(==) only value chek kore (===) value and type check kore
+const first = 0;
+const second = false;
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
