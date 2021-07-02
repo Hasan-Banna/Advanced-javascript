4756dc376171032322886e4f08f20b4a5ae4ce74
Hasan Al Banna <hasanbanna40@gmail.com>
2021-07-02 21:38:42 +0600

    null vs undefined interview qs

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+//undefined..
+//let name; ekhane kono value nai tai undefined dekhabe
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// } return na pele othoba khali return peleo hobe na
+
+//parameter na pele
+// function adda(num1,num2){
+//     console.log(num1, num2);
+// }
+// kono objecte je property ta nai seta khujle undefined hobe
+// const premik = {name:"smart dude", phone:0174566};
+// console.log(premik.gf);
+// const fun = undefined; variable er value undefined dile setao undefined dekhabe
+// kono kichur value null set kora thakle ba vule chilo ekhn null hoye gese seta null valu show korbe 
