b7708fcde0e8d53411503c4d69d0de367996a965
Hasan Al Banna <hasanbanna40@gmail.com>
2021-07-02 21:06:09 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+//0 pele false
+//"" false
+//undefined variable false
+//null pele false
+//NaN hole false
+//kono kisor value false hole false hobe
+//'0', ' ', [], {} true
+//
+let name = 0;
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
