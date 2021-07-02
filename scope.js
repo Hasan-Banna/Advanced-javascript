f3d5b1133075ddfd281f4d0c34f4394b6bac2891
Hasan Al Banna <hasanbanna40@gmail.com>
2021-07-02 23:38:59 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,18 @@
+let bonus = 20;
+
+function sum (num1, num2){
+    let result = num1 + num2 + bonus;
+    //console.log(result);
+    if(result > 9){
+        var mood = 'happy'; //let,const dile error ashbe but var dile error ashbe na.
+        mood = 'fishy';
+        mood = 'funky';
+        // console.log(mood); // if er baire hole error hobe.
+    }
+console.log(day); //var diye kore tar opor initialize korle undefined hobe let,const hole error ashbe.
+let day = "friday"; // let,const dile error ashbe
+
+    return result;
+}
+  const output = sum(3, 7);
+//  console.log(output);
